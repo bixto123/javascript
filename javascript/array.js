59f66c293656248b5ed3671b54195540dd6bda02
bixto123 <bixto@hotmail.com>
2022-11-15 13:03:09 +0100

    modification de array.js

--- javascript/array.js
+++ javascript/array.js
@@ -1,20 +1,21 @@
-/* affiche le contenu d'un array */
+/* affiche le contenu d'un tableau */
 function print(arr) {
     arr.forEach((element, index) => {
         console.log(index + ": " + element)
     });    
 }
 
-/* déclare et assign un array fruit */
+/* déclare et assign un tableau fruit */
 const fruit = ["Bannane", "Melon", "Poire", "Pomme"];
 
-/* déclare et assign un array fruit1 avec une list de ces éléments */
+/* déclare et assign un tableau list avec une list indexé de ces éléments */
 const list = fruit.map(function (element, index) {
     element = "<li>" + index + ": " + element + "</li>";
     return element;
 });
 
-console.log("Object fruit:");
+/* affiche le contenu des tableaux */
+console.log("Tableau fruit:");
 console.log(fruit);
 console.log("Contenu de fruit:");
 print(fruit);
