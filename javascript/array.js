4872943f3c2272afa5fd71c117a6ecb271ad5330
bixto123 <bixto@hotmail.com>
2022-11-15 12:53:17 +0100

    modification de main.html et ajout de array.js

---
+++ javascript/array.js
@@ -0,0 +1,22 @@
+/* affiche le contenu d'un array */
+function print(arr) {
+    arr.forEach((element, index) => {
+        console.log(index + ": " + element)
+    });    
+}
+
+/* déclare et assign un array fruit */
+const fruit = ["Bannane", "Melon", "Poire", "Pomme"];
+
+/* déclare et assign un array fruit1 avec une list de ces éléments */
+const list = fruit.map(function (element, index) {
+    element = "<li>" + index + ": " + element + "</li>";
+    return element;
+});
+
+console.log("Object fruit:");
+console.log(fruit);
+console.log("Contenu de list:");
+print(list);
+console.log("Contenu de fruit1:");
+print(list);
