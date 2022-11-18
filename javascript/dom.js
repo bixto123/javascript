8dd592fc5ea301e0c11f68ec354cf546f6762158
bixto123 <bixto@hotmail.com>
2022-11-18 11:39:19 +0100

    changement dans dom.js

--- javascript/dom.js
+++ javascript/dom.js
@@ -1,4 +1,4 @@
-// Création d'un object littéral
+//  Création d'un object littéral
 const human = {
   Name: "Fabrice",
   Lastname: "Dame",
@@ -7,23 +7,25 @@ const human = {
   Gender: "Male"
 }
 
-// Affiche le contenu de l'object littéral human
+//  Affiche le contenu de l'object littéral human
 console.log(human);
 
-// Obtient un élement par son selecteur comme en css
+//  Obtient un élement par son selecteur comme en css
 const ul1 = document.querySelector("#ul1");
+
+//  Definit le style css de ul1
 ul1.parentElement.style.listStyleType = "none";
 ul1.parentElement.style.padding = "0rem";
 ul1.parentElement.style.margin = "0rem";
 
-// Création d'un élement <li></li> pour chaque élément de l'object littéral human
+//  Création d'un élement <li></li> pour chaque élément de l'object littéral human
 for (const element in human) {
   const li = document.createElement("li");
   li.textContent = element.valueOf() + ": " + human[element.valueOf()];
   ul1.appendChild(li);
 }
 
-// Affichage d'un bouton clickable qui affiche bonjour le monde
+//  Affichage d'un bouton clickable qui affiche bonjour le monde
 let flag = false;
 const button1 = document.querySelector("#button1").parentElement;
 button1.onclick = function (e) {
@@ -40,23 +42,29 @@ button1.onclick = function (e) {
 }
 
 // Affichage d'un formulaire input et de son contenu quand le bouton est clické
+
+// Création et configuration d'un élément label */
 const label = document.createElement("label");
 label.setAttribute("for", "name");
 label.textContent = "Nom:";
 label.style.marginRight = "1rem";
 
+//  Création et configuration d'un élément input de type text
 const input = document.createElement("input");
 input.setAttribute("type", "text");
 input.setAttribute("id", "name");
 input.style.marginRight = "1rem";
 
+//  Création et configuration d'un élément button
 const button2 = document.createElement("button");
 button2.textContent = "Afficher le contenu du fomulaire";
 
+//  Création et configuration d'un élément p
 const paragraph8 = document.createElement("p");
 paragraph8.textContent = "";
 paragraph8.style.color = "red";
 
+//  Requière l'élement .section et lui ajoute les éléments label, input, button2 et paragraph8
 const section = document.querySelector("#section");
 section.appendChild(label);
 section.appendChild(input);
