b35e45d117494296cb090b4ab2b3e1a7e9d95afc
bixto123 <bixto@hotmail.com>
2022-11-27 23:36:58 +0100

    modification de promise.js et ajout de fetch.html nahida_draw.png fetch.js

---
+++ javascript/fetch.js
@@ -0,0 +1,10 @@
+const myImage = document.querySelector('img');
+
+fetch("../images/nahida_draw.png")
+.then(function(response) {
+  return response.blob();
+})
+.then(function(myBlob) {
+  const objectURL = URL.createObjectURL(myBlob);
+  myImage.src = objectURL;
+});
